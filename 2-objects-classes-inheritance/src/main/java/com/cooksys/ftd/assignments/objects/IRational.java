744e2eeebacace4dc2cf2dc159094f06c082ebe2
LaVette Clifton <lavette.clifton@gmail.com>
2017-06-02 00:51:38 -0500

    Rationals: implemented sub() method

--- 2-objects-classes-inheritance/src/main/java/com/cooksys/ftd/assignments/objects/IRational.java
+++ 2-objects-classes-inheritance/src/main/java/com/cooksys/ftd/assignments/objects/IRational.java
@@ -88,7 +88,13 @@ interface IRational {
      * @throws IllegalArgumentException if that is null
      */
     default IRational sub(IRational that) throws IllegalArgumentException {
-        throw new NotImplementedException();
+    	if (that == null) {
+    		throw new IllegalArgumentException();
+    	}
+    	else {
+    		return new Rational(((this.getNumerator() * that.getDenominator()) - (that.getNumerator() * this.getDenominator())), 
+    				(this.getDenominator() * that.getDenominator()));
+    	}
     }
 
     /**
